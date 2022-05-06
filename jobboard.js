facdb19a447727646ce93fcaa51822e4aa624cfb
Ethan <ehancoc4@kent.edu>
2022-05-05 23:58:43 -0400

    kaitlin's documentation

--- jobboard.js
+++ jobboard.js
@@ -118,6 +118,7 @@ function getJobs(rss) {
 }
 
 function showFullTime() {
+  document.getElementsByClassName("display-jobs")[0].innerHTML = "";
   if (document.getElementById("full-time").classList.contains("hide")) {
     document.getElementById("full-time").classList.remove("hide");
     document.getElementById("full-time").classList.add("show");
@@ -131,6 +132,7 @@ function showFullTime() {
   document.getElementById("pti-button").classList.remove("active");
 }
 function showFullTimeInternship() {
+  document.getElementsByClassName("display-jobs")[0].innerHTML = "";
   if (
     document.getElementById("full-time-internship").classList.contains("hide")
   ) {
@@ -147,6 +149,7 @@ function showFullTimeInternship() {
 }
 
 function showPartTime() {
+  document.getElementsByClassName("display-jobs")[0].innerHTML = "";
   if (document.getElementById("part-time").classList.contains("hide")) {
     document.getElementById("part-time").classList.remove("hide");
     document.getElementById("part-time").classList.add("show");
